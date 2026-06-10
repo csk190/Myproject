eb63755f6c984ee7692cbe78f47adad93d20f44a
csk190 <csk190@gmail.com>
2026-06-10 18:19:18 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ import streamlit as st
 st.markdown("#### Spring 2026")
 st.caption("This page is continuously updated.")
 
-IMAGE_URL = "https://raw.githubusercontent.com/csk190/miniproject/main/images/Gemini_Generated_Image_q5723kq5723kq572.png"
+IMAGE_URL = "https://raw.githubusercontent.com/csk190/Myproject/blob/main/images/Gemini_Generated_Image_4k37ym4k37ym4k37.png"
 
 # 파일 구조가 Home.py와 같은 위치에 images 폴더가 있다면
 st.image(
